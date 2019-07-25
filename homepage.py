01bb5fbf0fea8b842aa19508c824dbef022700da
BENXBEN <32802278+ahmedsifbenmessaoud@users.noreply.github.com>
2019-07-25 15:02:31 +0100

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'homepage.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(752, 594)
+        Dialog.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(90, 550, 621, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 200, 181, 20))
+        self.label.setObjectName("label")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(Dialog)
+        self.plainTextEdit.setGeometry(QtCore.QRect(210, 170, 341, 71))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(40, 450, 141, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(60, 300, 121, 31))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(210, 270, 341, 91))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit_2 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_2.setGeometry(QtCore.QRect(210, 410, 341, 131))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.textEdit_3 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_3.setGeometry(QtCore.QRect(610, 270, 104, 31))
+        self.textEdit_3.setObjectName("textEdit_3")
+        self.textEdit_4 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_4.setGeometry(QtCore.QRect(610, 370, 104, 31))
+        self.textEdit_4.setObjectName("textEdit_4")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(580, 230, 171, 20))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(580, 320, 171, 41))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(630, 250, 71, 17))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(630, 350, 71, 17))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(210, 150, 251, 17))
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(Dialog)
+        self.label_7.setGeometry(QtCore.QRect(210, 250, 341, 17))
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(210, 390, 341, 17))
+        self.label_8.setObjectName("label_8")
+        self.label_13 = QtWidgets.QLabel(Dialog)
+        self.label_13.setGeometry(QtCore.QRect(200, 80, 361, 41))
+        self.label_13.setStyleSheet("font: 57 20pt \"Ubuntu\";")
+        self.label_13.setObjectName("label_13")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Enter your DNA sequence:"))
+        self.pushButton.setText(_translate("Dialog", "Get multipeptide:"))
+        self.pushButton_3.setText(_translate("Dialog", "Get RNA:"))
+        self.label_2.setText(_translate("Dialog", "percentage of C and G"))
+        self.label_3.setText(_translate("Dialog", "percentage of A and U"))
+        self.label_4.setText(_translate("Dialog", "in RNA"))
+        self.label_5.setText(_translate("Dialog", "in RNA"))
+        self.label_6.setText(_translate("Dialog", "Your DNA sequence:"))
+        self.label_7.setText(_translate("Dialog", "Your RNA sequence:"))
+        self.label_8.setText(_translate("Dialog", "Your multipeptide sequence is:"))
+        self.label_13.setText(_translate("Dialog", "DNA, RNA, Protien Translator "))
+
